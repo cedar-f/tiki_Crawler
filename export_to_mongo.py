6de91be7e16beed44de214fd3082c25dd640a586
cedar-f <dotunglamnd@gmail.com>
2020-09-17 16:42:36 +0700

    change database name

--- export_to_mongo.py
+++ export_to_mongo.py
@@ -9,6 +9,6 @@ class Export:
         self.mongo = mg.MongoClient("localhost", 27017)
 
     def to_mongo(self, product):
-        mydb = self.mongo["mydatabase"]
+        mydb = self.mongo["Crawl_data"]
         mycol = mydb["tiki"]
-        x = mycol.insert(product,check_keys=False)
+        x = mycol.insert(product, check_keys=False)
